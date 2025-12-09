69538a9ef73bb242d940e41674ced9631bbf5018
Alexander Yang <65861156+blueputty01@users.noreply.github.com>
2025-12-09 17:04:41 -0500

    make db dynamic to service names and number

--- internal/sync/db.go
+++ internal/sync/db.go
@@ -4,18 +4,23 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"github.com/blueputty01/task-sync/internal/utils"
 	_ "github.com/mattn/go-sqlite3"
-	"iter"
 	"log/slog"
 	"strings"
 	"time"
 )
 
-type DB struct {
+var TableName = "sync_items"
+var ColumnSuffix = "_id"
+
+type DBClient struct {
 	*sql.DB
+	services    []string
+	columnNames []string
 }
 
-func NewDB(dataSourceName string) (*DB, error) {
+func NewDB(dataSourceName string, services []string) (*DBClient, error) {
 	db, err := sql.Open("sqlite3", dataSourceName)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open database: %w", err)
@@ -24,37 +29,62 @@ func NewDB(dataSourceName string) (*DB, error) {
 		return nil, fmt.Errorf("failed to ping database: %w", err)
 	}
 
-	if err := createSchema(db); err != nil {
+	if err := createSchema(db, services); err != nil {
 		return nil, fmt.Errorf("failed to create schema: %w", err)
 	}
 
-	return &DB{db}, nil
+	return &DBClient{db, services, servicesWithSuffix(services)}, nil
 }
 
-func createSchema(db *sql.DB) error {
-	query := `
-	CREATE TABLE IF NOT EXISTS sync_items (
+func servicesWithSuffix(services []string) []string {
+	suffixedServices := make([]string, len(services))
+	for i, service := range suffixedServices {
+		suffixedServices[i] = service + ColumnSuffix
+	}
+	return suffixedServices
+}
+
+// TODO consider making this table dynamic based on the services used
+func createSchema(db *sql.DB, services []string) error {
+	suffixedServices := servicesWithSuffix(services)
+	serviceColumns := make([]string, len(services))
+	for i, suffixedService := range suffixedServices {
+		serviceColumns[i] = fmt.Sprintf("%s TEXT UNIQUE", suffixedService)
+	}
+
+	mainTable := fmt.Sprintf(`
+	CREATE TABLE IF NOT EXISTS %s (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		summary TEXT,
-		cal_id TEXT UNIQUE,
-		yt_id TEXT UNIQUE
+		%s
 	);
+	`, TableName, strings.Join(serviceColumns, ", "))
 
+	quotedServices := make([]string, len(services))
+	for i, service := range services {
+		quotedServices[i] = fmt.Sprintf("'%s'", service)
+	}
+	updateTable := fmt.Sprintf(`
 	CREATE TABLE IF NOT EXISTS last_sync (
 		service TEXT PRIMARY KEY,
-		timestamp INTEGER
+		timestamp INTEGER,
+	    CHECK ( timestamp >= 0 ),
+		CHECK ( service IN (%s))
 	);
+	`, strings.Join(quotedServices, ", "))
 
-	CREATE INDEX IF NOT EXISTS idx_sync_items_cal_id ON sync_items (cal_id);
-	CREATE INDEX IF NOT EXISTS idx_sync_items_yt_id ON sync_items (yt_id);
-	`
-	_, err := db.Exec(query)
+	indexStrings := make([]string, len(suffixedServices))
+	for i, service := range suffixedServices {
+		indexStrings[i] = fmt.Sprintf("CREATE INDEX IF NOT EXISTS idx_sync_items_%s_id ON sync_items (%s)", service)
+	}
+	indices := strings.Join(indexStrings, "; ")
+	_, err := db.Exec(mainTable + updateTable + indices)
 	return err
 }
 
-func (db *DB) GetLastSyncTimestamp(service string) (time.Time, error) {
+func (c *DBClient) GetLastSyncTimestamp(service string) (time.Time, error) {
 	var rawTime int64
-	err := db.QueryRow("SELECT timestamp FROM last_sync WHERE service = ?", service).Scan(&rawTime)
+	err := c.QueryRow("SELECT timestamp FROM last_sync WHERE service = ?", service).Scan(&rawTime)
 
 	if errors.Is(err, sql.ErrNoRows) {
 		slog.Warn("No last sync timestamp found, returning zero time", slog.String("service", service))
@@ -66,29 +96,24 @@ func (db *DB) GetLastSyncTimestamp(service string) (time.Time, error) {
 	return time.Unix(rawTime, 0), nil
 }
 
-func (db *DB) UpdateLastSyncTimestamp(service string, timestamp int64) error {
-	_, err := db.Exec("INSERT INTO last_sync (service, timestamp) VALUES (?, ?) ON CONFLICT(service) DO UPDATE SET timestamp = excluded.timestamp", service, timestamp)
+func (c *DBClient) UpdateLastSyncTimestamp(service string, timestamp int64) error {
+	_, err := c.Exec("INSERT INTO last_sync (service, timestamp) VALUES (?, ?) ON CONFLICT(service) DO UPDATE SET timestamp = excluded.timestamp", service, timestamp)
 	if err != nil {
 		return fmt.Errorf("failed to update last sync timestamp: %w", err)
 	}
 	return nil
 }
 
-func isValidColumn(col string) bool {
-	validColumns := map[string]bool{
-		"cal_id":  true,
-		"yt_id":   true,
-		"summary": true,
-	}
-	return validColumns[col]
+func (c *DBClient) isValidColumn(col string) bool {
+	return utils.ArrayContains(c.services, col[:len(col)-len(ColumnSuffix)])
 }
 
-func (db *DB) UpdateIssue(service, id, summary string) error {
-	if !isValidColumn(service) {
+func (c *DBClient) UpdateIssue(service, id, summary string) error {
+	if !c.isValidColumn(service) {
 		return fmt.Errorf("invalid service column: %s", service)
 	}
 
-	_, err := db.Exec("INSERT INTO sync_items (?, summary) VALUES (?, ?) ON CONFLICT(?) DO UPDATE SET summary = excluded.summary", service, id, summary, service)
+	_, err := c.Exec("INSERT INTO sync_items (?, summary) VALUES (?, ?) ON CONFLICT(?) DO UPDATE SET summary = excluded.summary", service, id, summary, service)
 	if err != nil {
 		return fmt.Errorf("failed to update calendar event: %w", err)
 	}
@@ -96,14 +121,13 @@ func (db *DB) UpdateIssue(service, id, summary string) error {
 }
 
 type DBItem struct {
-	CalID   string
-	YtID    string
+	ids     map[string]string
 	Summary string
 }
 
 // GetItems retrieves items that exist in potentially multiple services based on the provided serviceItems map.
-func (db *DB) GetItems(serviceItems map[string][]ItemsToUpdate) ([]DBItem, error) {
-	query := "SELECT cal_id, yt_id, summary FROM sync_items WHERE"
+func (c *DBClient) GetItems(serviceItems map[string][]ItemsToUpdate) ([]DBItem, error) {
+	query := fmt.Sprintf("SELECT %s, summary FROM sync_items WHERE", strings.Join(c.columnNames, ", "))
 
 	var queryBuilder strings.Builder
 
@@ -123,7 +147,7 @@ func (db *DB) GetItems(serviceItems map[string][]ItemsToUpdate) ([]DBItem, error
 		idx += 1
 	}
 
-	rows, err := db.Query(query)
+	rows, err := c.Query(query)
 	if err != nil {
 		return nil, fmt.Errorf("failed to query overlap serviceItems: %w", err)
 	}
@@ -135,9 +159,30 @@ func (db *DB) GetItems(serviceItems map[string][]ItemsToUpdate) ([]DBItem, error
 	}(rows)
 
 	var overlapItems []DBItem
+
+	columns, err := rows.Columns()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get columns: %w", err)
+	}
+
+	// Create a slice of interface{} pointers for scanning
+	// Allocate one for each column
+	scanValues := make([]interface{}, len(columns))
+	for i := range scanValues {
+		scanValues[i] = new(sql.NullString)
+	}
+
 	for rows.Next() {
 		var item DBItem
-		if err := rows.Scan(&item.CalID, &item.YtID, &item.Summary); err != nil {
+
+		item.ids = make(map[string]string)
+		ids := make([]string, len(c.services))
+
+		for idx, col := range c.columnNames {
+			item.ids[col] = ids[idx]
+		}
+
+		if err := rows.Scan(ids, &item.Summary); err != nil {
 			return nil, fmt.Errorf("failed to scan overlap item: %w", err)
 		}
 		overlapItems = append(overlapItems, item)
