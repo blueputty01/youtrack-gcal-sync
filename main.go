269c84a2c2f7645c7d4ca1d7464fad797ed0800e
Alexander Yang <65861156+blueputty01@users.noreply.github.com>
2025-12-09 18:20:15 -0500

    add base sync calls

--- main.go
+++ main.go
@@ -1,15 +1,42 @@
 package main
 
 import (
+	"github.com/blueputty01/task-sync/internal/calendar"
+	"github.com/blueputty01/task-sync/internal/projects"
 	"github.com/blueputty01/task-sync/internal/sync"
 	"log/slog"
+	"os"
 )
 
 func main() {
-	_, err := sync.NewSynchronizer("sync.db")
+	clients := []sync.Client{
+		constructYoutrackClient(),
+		constructGoogleCalendarClient(),
+	}
+	synchronizer, err := sync.NewSynchronizer("sync.db", clients)
 	if err != nil {
-		slog.Error("Failed to create synchronizer:", "DB error", err)
+		slog.Error("Failed to create synchronizer:", "DBClient error", err)
 	}
 
-	//synchronizer.Sync
+	slog.Info("Starting synchronization process")
+	err = synchronizer.Sync()
+	if err != nil {
+		slog.Error("Synchronization failed:", "Sync error", err)
+	}
+}
+
+func constructYoutrackClient() *projects.Client {
+	token, found := os.LookupEnv("YOUTRACK_TOKEN")
+	if !found {
+		slog.Error("YOUTRACK_TOKEN environment variable not set")
+	}
+
+	url, found := os.LookupEnv("YOUTRACK_URL")
+	if !found {
+		slog.Error("YOUTRACK_URL environment variable not set")
+	}
+	return projects.NewClient(url, token, nil)
+}
+func constructGoogleCalendarClient() *calendar.Client {
+	return calendar.NewClient()
 }
